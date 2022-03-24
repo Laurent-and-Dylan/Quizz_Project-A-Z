c522455b3bb8c67e6e9ffe266909cd4e45f47ee9
Dylan <dylan.castor06@gmail.com>
2022-03-24 17:16:09 +0100

    :truck: Add some quizz routes

--- back_end/controllers/quizz.controllers.js
+++ back_end/controllers/quizz.controllers.js
@@ -3,7 +3,6 @@ const Response = require("../models/response.model");
 const Quizz = require("../models/quizz.model");
 const { Op } = require("sequelize");
 const jwt = require("jsonwebtoken");
-const { raw } = require("express");
 
 //* @desc Création d'un quizz aléatoire
 //* @route GET /api/quizz/random
@@ -51,7 +50,7 @@ module.exports.getQuizz = async (req, res) => {
 
   //~ Requête récupérant le quizz par son id
   const quizz = await Quizz.findOne({ where: { id_quizz } });
-  if (quizz) return res.status(404).send("Quizz inconnu !");
+  if (!quizz) return res.status(404).send("Quizz inconnu !");
 
   //~ Requête pour récupéré les questions liées au quizz
   const questions = await Question.findAll({
@@ -85,28 +84,34 @@ module.exports.createQuizz = async (req, res) => {
   const token = req.cookies.jwt;
   const { quests, resps, name, id_category } = req.body;
   let id_quest = [];
-  let bin = [];
 
+  //~ Vérification de la possession du token JWT
   if (!token)
     return res
       .status(400)
       .send({ message: "Veuillez créer un compte ou vous connecter" });
 
+  //~ Vérification de la validité du token et récupération de l'id user
   const id_user = jwt.verify(token, process.env.SECRET_TOKEN)
     ? jwt.verify(token, process.env.SECRET_TOKEN).id
     : null;
 
   if (id_user) {
+    //~ Création du quizz si id utilisateur valide
     const quizz = await Quizz.findOrBuild({
+      //-- renvoie un tableau contenant le résultat et un boolean
       where: { name },
       defaults: { id_user, id_category, name },
       raw: true,
     });
 
     if (quizz[1]) {
-      await quizz[0].save();
+      await quizz[0].save(); //-- création du quizz
+
+      //~ Création des questions lié à l'id du quizz précédemment créé
+      //~ Boucle sur le tableau questions pour récupérer l'id de chacune et les inserer dans colonne id_quizz sous contrainte (FOREIGN KEY)
       for (q in quests) {
-        const quest = await Question.create(
+        const { id_question } = await Question.create(
           {
             id_quizz: quizz[0].id_quizz,
             id_category,
@@ -114,20 +119,94 @@ module.exports.createQuizz = async (req, res) => {
           },
           { raw: true }
         );
-        id_quest.push(quest.id_question);
+
+        //~ Mise dans un tableau des id de chaque question
+        id_quest.push(id_question);
       }
     }
 
+    //~ Création des réponses lié à chaque id de la question correspondante
+    //~ Boucle sur le tableau des id de questions puis sur celui du tableau des réponses
     for (id in id_quest) {
       for (asc in resps[id]) {
-        const resp = Response.create({
-          id_question: id_quest[id],
-          response: resps[id][asc][0],
-          value: resps[id][asc][1],
-        });
+        Response.create(
+          {
+            id_question: id_quest[id],
+            response: resps[id][asc][0],
+            value: resps[id][asc][1],
+          },
+          { raw: true }
+        );
       }
     }
+
+    res.status(201).send({
+      message: "Félicitation vous venez de créer votre nouveau Quizz !",
+    });
   } else {
-    res.status(400).send({ message: "Nom de quizz déjà existant" });
+    res.locals.user = null;
+    res.cookie("jwt", "", 0);
+    res.redirect("/").send({ message: "Invalid Token" });
+  }
+};
+
+//* @desc Suppression d'un quizz par un utilisateur ou un administrateur
+//* @route DELETE /api/quizz/delete/:id_quizz
+// ! Mettre en place l'authorisation admin
+
+module.exports.deleteQuizz = async (req, res) => {
+  const { id_quizz } = req.params;
+  const token = req.cookies.jwt;
+
+  //~ Vérification de la présence d'un cookie
+  if (!token)
+    return res
+      .status(400)
+      .send({ message: "You are not authorized to perform this operation" });
+
+  //~ Vérification de la validité du cookie plus récupération de l'id utilisateur
+  const { id } = jwt.verify(token, process.env.SECRET_TOKEN)
+    ? jwt.verify(token, process.env.SECRET_TOKEN)
+    : null;
+
+  //~ Requête pour récuperer l'id du créateur du quizz
+  const { id_user } = await Quizz.findOne({
+    where: { id_quizz },
+    attributes: ["id_user"],
+  });
+
+  //~ Structure de contrôle pour vérifiér le créateur du quizz et l'utilasateur faisant la requête
+  if (id === id_user) {
+    Quizz.destroy({ where: { id_quizz } });
+    return res.status(200).send({ message: "Your quizz has been deleted" });
+  } else {
+    res.status(400).send({ message: "An error has occurred" });
+  }
+};
+
+//* @desc Modification d'un quizz par un utilisateur
+//* @route PUT /api/quizz/edit/:id_quizz
+// ! Faire la vérification utilisateur plus structures de contrôle
+module.exports.editQuizz = async (req, res) => {
+  const { id_quizz } = req.params;
+  const { quests, resps, name } = req.body;
+
+  //~ Requête pour mettre à jour le nom du quizz
+  Quizz.update({ name }, { where: { id_quizz } });
+
+  //~ Requête pour mettre à jour les questions du quizz
+  for (q in quests) {
+    await Question.update(
+      { question: quests[q][0] },
+      { where: { id_question: [quests[q][1]] } }
+    );
+  }
+
+  //~ Requête pour mettre à jour les réponses de chaque question
+  for (r in resps) {
+    await Response.update(
+      { response: resps[r][0] },
+      { where: { id_response: [resps[r][1]] } }
+    );
   }
 };
--- back_end/routes/quizz.routes.js
+++ back_end/routes/quizz.routes.js
@@ -6,5 +6,10 @@ router.get("/random", QuizzController.random);
 router.get("/:id_quizz", QuizzController.getQuizz);
 router.get("/category/:id_category", QuizzController.quizzOfCategory);
 router.post("/create", QuizzController.createQuizz);
+router.delete("/delete/:id_quizz", QuizzController.deleteQuizz);
+router.put("/edit/:id_quizz", QuizzController.editQuizz);
 
 module.exports = router;
+
+//* @desc Suppression d'un quizz par un utilisateur ou un administrateur
+//* @route DELETE /api/quizz/delete/:id_quizz
