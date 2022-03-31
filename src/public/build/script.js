3694b8f13a10c64903fbb8fdd530d7987e1f1611
laurent7438 <1491laurent@gmail.com>
2022-03-31 09:48:29 +0200

    Burger finished

--- src/public/build/script.js
+++ src/public/build/script.js
@@ -4,15 +4,15 @@ import { Remover } from "./utils/Remover.js";
 import { Display as Forms } from "./views/Forms.views.js";
 import { accueil as Accueil } from "./views/Accueil.views.js";
 
-login.addEventListener("click", (e) => {
-  Remover.main();
-  Forms.loginForm();
-});
-signUp.addEventListener("click", (e) => {
-  Remover.main();
-  Forms.signUpForm();
-});
-guest.addEventListener("click", (e) => {
-  Remover.main();
-  Accueil();
-});
+// login.addEventListener("click", (e) => {
+//   Remover.main();
+//   Forms.loginForm();
+// });
+// signUp.addEventListener("click", (e) => {
+//   Remover.main();
+//   Forms.signUpForm();
+// });
+// guest.addEventListener("click", (e) => {
+//   Remover.main();
+//   Accueil();
+// });
